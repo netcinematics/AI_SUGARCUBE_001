8f3ccc600b648ffbdcafe6935007133450d7f3c9
NetCinematics <alpinefalcon@protonmail.com>
2026-01-21 20:01:55 -0800

    sub cubes

--- BRAIN_VIZ_CANVAS_2.js
+++ BRAIN_VIZ_CANVAS_2.js
@@ -31,74 +31,118 @@ export function initBrainViz() {
 
 
     // --- Helper Functions ---
-    function addDiagonalLines() {
-        const cubeSize = 2;
-        const halfSize = cubeSize / 2;
-        const lineLength = 9;
-        const corners = [
-            { start: [-halfSize, -halfSize, halfSize], dir: [-1, -1, 1] },
-            { start: [halfSize, -halfSize, halfSize], dir: [1, -1, 1] },
-            { start: [-halfSize, halfSize, halfSize], dir: [-1, 1, 1] },
-            { start: [halfSize, halfSize, halfSize], dir: [1, 1, 1] },
-            { start: [-halfSize, -halfSize, -halfSize], dir: [-1, -1, -1] },
-            { start: [halfSize, -halfSize, -halfSize], dir: [1, -1, -1] },
-            { start: [-halfSize, halfSize, -halfSize], dir: [-1, 1, -1] },
-            { start: [halfSize, halfSize, -halfSize], dir: [1, 1, -1] }
-        ];
+    // function addDiagonalLines() {
+    //     const cubeSize = 2;
+    //     const halfSize = cubeSize / 2;
+    //     const lineLength = 9;
+    //     const corners = [
+    //         { start: [-halfSize, -halfSize, halfSize], dir: [-1, -1, 1] },
+    //         { start: [halfSize, -halfSize, halfSize], dir: [1, -1, 1] },
+    //         { start: [-halfSize, halfSize, halfSize], dir: [-1, 1, 1] },
+    //         { start: [halfSize, halfSize, halfSize], dir: [1, 1, 1] },
+    //         { start: [-halfSize, -halfSize, -halfSize], dir: [-1, -1, -1] },
+    //         { start: [halfSize, -halfSize, -halfSize], dir: [1, -1, -1] },
+    //         { start: [-halfSize, halfSize, -halfSize], dir: [-1, 1, -1] },
+    //         { start: [halfSize, halfSize, -halfSize], dir: [1, 1, -1] }
+    //     ];
 
-        corners.forEach(corner => {
-            const lineMaterial = new THREE.LineBasicMaterial({ color: 0x00FFFF });
-            const points = [];
-            points.push(new THREE.Vector3(...corner.start));
-            points.push(new THREE.Vector3(
-                corner.start[0] + corner.dir[0] * lineLength,
-                corner.start[1] + corner.dir[1] * lineLength,
-                corner.start[2] + corner.dir[2] * lineLength
-            ));
-            const lineGeometry = new THREE.BufferGeometry().setFromPoints(points);
-            const line = new THREE.Line(lineGeometry, lineMaterial);
-            scene.add(line);
-        });
-    }
-    addDiagonalLines();
+    //     corners.forEach(corner => {
+    //         const lineMaterial = new THREE.LineBasicMaterial({ color: 0x00FFFF });
+    //         const points = [];
+    //         points.push(new THREE.Vector3(...corner.start));
+    //         points.push(new THREE.Vector3(
+    //             corner.start[0] + corner.dir[0] * lineLength,
+    //             corner.start[1] + corner.dir[1] * lineLength,
+    //             corner.start[2] + corner.dir[2] * lineLength
+    //         ));
+    //         const lineGeometry = new THREE.BufferGeometry().setFromPoints(points);
+    //         const line = new THREE.Line(lineGeometry, lineMaterial);
+    //         scene.add(line);
+    //     });
+    // }
+    // addDiagonalLines();
 
-    function add_ORBZ_1() {
+    function add_Sphere_Anchors() {
         const points_NUM_1 = 10;//20;
         const points = [
             { x: points_NUM_1, y: points_NUM_1, z: points_NUM_1 }, { x: -points_NUM_1, y: points_NUM_1, z: points_NUM_1 }, { x: -points_NUM_1, y: -points_NUM_1, z: points_NUM_1 }, { x: -points_NUM_1, y: -points_NUM_1, z: -points_NUM_1 },
             { x: points_NUM_1, y: points_NUM_1, z: -points_NUM_1 }, { x: points_NUM_1, y: -points_NUM_1, z: -points_NUM_1 }, { x: -points_NUM_1, y: points_NUM_1, z: -points_NUM_1 }, { x: points_NUM_1, y: -points_NUM_1, z: points_NUM_1 }
         ];
-
         let sphereGeometry = new THREE.SphereGeometry(1.222, 32, 32);
-        let sphereMaterial = new THREE.MeshBasicMaterial({ color: 0x00FFFF, transparent: true, opacity: 0.8 });
+        let sphereMaterial = new THREE.MeshBasicMaterial({ color: 0x39FF8D, transparent: true, opacity: 0.8 });
         let sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
         sphere.position.set(0, 0, 0);
         scene.add(sphere);
-
         sphereGeometry = new THREE.SphereGeometry(0.666, 32, 32);
         for (let i = 0; i < points.length; i++) {
             sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
             sphere.position.set(points[i].x, points[i].y, points[i].z);
             scene.add(sphere);
         }
+    }
 
-        //init - ORBZ
-        let aMIN = -10; let aMAX = 10;
-        sphereGeometry = new THREE.SphereGeometry(0.111, 32, 32);
-        sphereMaterial = new THREE.MeshBasicMaterial({ color: 0x00FFFF, transparent: true, opacity: 0.222 });
+    function add_Sugar_ORBZ_1(aMIN, aMAX) {
+        //---- ****** ORBZ ****** ----
+        // Set this for the dimensions of the sugarcube.
+        // let aMIN = -10; let aMAX = 10;
+        const sphereGeometry = new THREE.SphereGeometry(0.111, 32, 32);
+        const sphereMaterial = new THREE.MeshBasicMaterial({ color: 0x00FFFF, transparent: true, opacity: 0.222 });
+
         for (let x = aMIN; x < aMAX; x++) {
             for (let y = aMIN; y < aMAX; y++) {
                 for (let z = aMIN; z < aMAX; z++) {
-                    sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+                    const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
                     sphere.position.set(x, y, z);
                     scene.add(sphere);
                 }
             }
         }
     }
-    add_ORBZ_1();
 
-    function addTesseract_1() {
+    // function add_Sugar_ORBZ_2(XDIM, YDIM, ZDIM) {
+    function add_SUB_CUBE_3(sub_cube) {
+        //---- ****** ORBZ ****** ----
+        // Set this for the dimensions of the sugarcube.
+        const sphereGeometry = new THREE.SphereGeometry(0.111, 32, 32);
+        // const sphereMaterial = new THREE.MeshBasicMaterial({ color: 0x00FFFF, transparent: true, opacity: 0.222 });
+        const sphereMaterial = new THREE.MeshBasicMaterial({ color: sub_cube.color, transparent: true, opacity: 0.222 });
+
+        // let XDIM = 4, YDIM = 4, ZDIM = 4;
+        //CONTAINS SUB-CUBE OFFSET inside the iterater definition to give block_spike offset.
+        for (let x = sub_cube.XDIM > 0 ? 1 : -1; x !== sub_cube.XDIM; sub_cube.XDIM > 0 ? x++ : x--) {
+            for (let y = sub_cube.XDIM > 0 ? 1 : -1; y !== sub_cube.YDIM; sub_cube.YDIM > 0 ? y++ : y--) {
+                for (let z = sub_cube.XDIM > 0 ? 1 : -1; z !== sub_cube.ZDIM; sub_cube.ZDIM > 0 ? z++ : z--) {
+                    const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+                    sphere.position.set(x, y, z);
+                    scene.add(sphere);
+                }
+            }
+        }
+    }
+
+    // function add_ORBZ_1() {
+    // add_Sugar_ORBZ_1(-3, 3);  //MIN and MAX values for the ORBZ.
+    // add_Sugar_ORBZ_2(4, 4, 4);  //MIN and MAX values for the ORBZ.
+    // }|
+    function add_SUB_CUBES_1() {
+        // add_Sugar_ORBZ_1(-3, 3);  //MIN and MAX values for the ORBZ.
+        // add_Sugar_ORBZ_2(4, 4, 4);  //MIN and MAX values for the ORBZ.
+        add_SUB_CUBE_3({ XDIM: 4, YDIM: 4, ZDIM: 4, color: 0x00FFFF });  //MIN and MAX values for the ORBZ.
+        add_SUB_CUBE_3({ XDIM: -4, YDIM: -4, ZDIM: -4, color: 0x00FFFF });  //MIN and MAX values for the ORBZ.
+        add_SUB_CUBE_3({ XDIM: 4, YDIM: 4, ZDIM: -4, color: 0x00FFFF });  //MIN and MAX values for the ORBZ.
+        add_SUB_CUBE_3({ XDIM: 4, YDIM: -4, ZDIM: -4, color: 0x00FFFF });  //MIN and MAX values for the ORBZ.
+
+        add_SUB_CUBE_3({ XDIM: 4, YDIM: -4, ZDIM: 4, color: 0x00FFFF });
+        add_SUB_CUBE_3({ XDIM: -4, YDIM: 4, ZDIM: 4, color: 0x00FFFF });
+        add_SUB_CUBE_3({ XDIM: -4, YDIM: 4, ZDIM: -4, color: 0x00FFFF });
+        add_SUB_CUBE_3({ XDIM: -4, YDIM: -4, ZDIM: 4, color: 0x00FFFF });
+        // add_SUB_CUBE_3({ XDIM: -4, YDIM: -4, ZDIM: -4, color: 0x00FFFF });  //MIN and MAX values for the ORBZ.
+        // add_Sugar_ORBZ_1(-10, 10);  //MIN and MAX values for the ORBZ.
+        // add_Sphere_Anchors();
+    }
+    add_SUB_CUBES_1();
+
+    function add_Tesseract_1() {
         const geometry = new THREE.BoxGeometry(2, 2, 2);
         const material = new THREE.MeshBasicMaterial({ color: 0x4682B4, transparent: true, opacity: 0.9 });
         const box = new THREE.Mesh(geometry, material);
@@ -140,9 +184,9 @@ export function initBrainViz() {
             }
         }
     }
-    addTesseract_1();
+    // add_Tesseract_1();
 
-    function addDiagonalBoxes() {
+    function add_Anchor_Boxes() {
         const xcubeSize = 1;
         const xsquareGeometry = new THREE.BoxGeometry(xcubeSize, xcubeSize, xcubeSize);
         const xsquareMaterial = new THREE.MeshBasicMaterial({ color: 0x4682B4, transparent: true, opacity: 0.8 });
@@ -158,7 +202,7 @@ export function initBrainViz() {
             scene.add(xSquare);
         });
     }
-    addDiagonalBoxes();
+    // add_Anchor_Boxes();
 
     const points = [
         { x: 0, y: 10, z: 0, txt: 'up', color: 0xFFFF00 },
@@ -188,47 +232,58 @@ export function initBrainViz() {
         }
     }
 
-    const sphereGeometry = new THREE.SphereGeometry(0.3, 32, 32);
-    const spheres = new Map();
-    const raycaster = new THREE.Raycaster();
-    const mouse = new THREE.Vector2();
+    function add_Dimension_Spikes() {
+        const sphereGeometry = new THREE.SphereGeometry(0.3, 32, 32);
+        const spheres = new Map();
+        const raycaster = new THREE.Raycaster();
+        const mouse = new THREE.Vector2();
 
-    points.forEach(point => {
-        const sphereMaterial = new THREE.MeshBasicMaterial({ color: point.color });
-        const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
-        sphere.position.set(point.x, point.y, point.z);
-        scene.add(sphere);
-        spheres.set(sphere.id, point);
+        points.forEach(point => {
+            const sphereMaterial = new THREE.MeshBasicMaterial({ color: point.color });
+            const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+            sphere.position.set(point.x, point.y, point.z);
+            scene.add(sphere);
+            spheres.set(sphere.id, point);
 
-        createIntermediateSpheres(
-            { x: 0, y: 0, z: 0 },
-            { x: point.x, y: point.y, z: point.z },
-            point.color
-        );
+            createIntermediateSpheres(
+                { x: 0, y: 0, z: 0 },
+                { x: point.x, y: point.y, z: point.z },
+                point.color
+            );
 
-        const canvas = document.createElement('canvas');
-        const context = canvas.getContext('2d');
-        canvas.width = 256;
-        canvas.height = 256;
-        context.font = '44px Impact';
-        context.fillStyle = 'aqua';
-        context.textAlign = 'center';
-        context.fillText(point.txt, 128, 128);
+            const canvas = document.createElement('canvas');
+            const context = canvas.getContext('2d');
+            canvas.width = 256;
+            canvas.height = 256;
+            context.font = '44px Impact';
+            context.fillStyle = 'aqua';
+            context.textAlign = 'center';
+            context.fillText(point.txt, 128, 128);
 
-        const texture = new THREE.Texture(canvas);
-        texture.needsUpdate = true;
+            const texture = new THREE.Texture(canvas);
+            texture.needsUpdate = true;
 
-        const spriteMaterial = new THREE.SpriteMaterial({ map: texture });
-        const sprite = new THREE.Sprite(spriteMaterial);
-        sprite.position.set(point.x, point.y + 1, point.z);
-        sprite.scale.set(4, 2, 1);
-        scene.add(sprite);
-    });
+            const spriteMaterial = new THREE.SpriteMaterial({ map: texture });
+            const sprite = new THREE.Sprite(spriteMaterial);
+            sprite.position.set(point.x, point.y + 1, point.z);
+            sprite.scale.set(4, 2, 1);
+            scene.add(sprite);
+        });
+    }
+    add_Dimension_Spikes();
 
+    // **** --- INITIALIZE SUGARCUBE VIZUALIZATION ---- **** //
     camera.position.set(15, 15, -15);
     camera.lookAt(0, 0, 0);
 
-    // --- Interaction ---
+    function animate() {
+        requestAnimationFrame(animate);
+        controls.update();
+        renderer.render(scene, camera);
+    }
+    animate();
+
+    // *******--- Interaction --- ***************************//
     // Note: Interaction (click) needs to listen on the rendered canvas
     function onClick(event) {
         const rect = renderer.domElement.getBoundingClientRect();
@@ -252,13 +307,6 @@ export function initBrainViz() {
         }
     }
     renderer.domElement.addEventListener('click', onClick, false);
-
-    function animate() {
-        requestAnimationFrame(animate);
-        controls.update();
-        renderer.render(scene, camera);
-    }
-    animate();
 
     function onWindowResize() {
         if (!container) return;
